a04bc4c96f356a9d4d37f3af4ac2b41fcbf1ce6e
Girish <girish.soman@schbang.com>
2025-04-26 14:01:51 +0530

    refactor: remove unnecessary blank line in User.controller.js

--- controllers/User.controller.js
+++ controllers/User.controller.js
@@ -21,7 +21,6 @@ const generateOTP = () => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
 const createUser = (userData) => {
     const { fullName, email, password } = userData;
     const registrationOtp = generateOTP();

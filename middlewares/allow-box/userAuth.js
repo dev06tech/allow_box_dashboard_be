01146f90ad49f74b7aacd22e1503790a7b0339dd
dev08.tech <dev08.tech@gmail.com>
2025-05-18 23:21:15 +0530

    fix:added resetPasswordAuth

--- middlewares/allow-box/userAuth.js
+++ middlewares/allow-box/userAuth.js
@@ -110,7 +110,55 @@ const userAuth = async (req, res, next) => {
     }
 };
 
+const resetPasswordAuth = async (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(httpStatus.UNAUTHORIZED).json({
+            message: 'Authentication required. Please provide a valid Bearer token.'
+        });
+    }
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+        return res.status(httpStatus.UNAUTHORIZED).json({ message: 'No token provided' });
+    }
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, config.jwt.secret);
+        const user = await User.findById(decoded._id);
+        if (!user) {
+            return res.status(httpStatus.NOT_FOUND).json({ message: 'User not found' });
+        }
+        if(!user.isEmailVerified){
+            return res.status(httpStatus.BAD_REQUEST).json({ message: 'Email not verified, Please verify your email' });
+        }
+        req.user = user;
+        req.token = token;
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            const decoded = jwt.decode(token); // decode without verifying
+            const user = await User.findById(decoded?._id);
+
+            if (user) {
+                user.tokens = []
+                user.isLoggedIn = false;
+                await user.save();
+            }
+
+            return res.status(httpStatus.UNAUTHORIZED).json({
+                message: 'Token expired',
+                logout: true
+            });
+        }
+        return res.status(httpStatus.UNAUTHORIZED).json({
+            message: 'Invalid token',
+            logout: true
+        });
+    }
+};
+
 module.exports = {
     userAuth,
-    isRegisteredUser
+    isRegisteredUser,
+    resetPasswordAuth
 };

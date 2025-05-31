990d2ac129d28b1af57006b6f0d0a4590a497b21
dev06tech <dev08.tech@gmail.com>
2025-05-31 18:50:43 +0530

    fix:added staff role

--- models/allow-box/user.model.js
+++ models/allow-box/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ["super-admin", "teacher", "student", "parent", "support"],
+        enum: ["super-admin", "teacher", "student", "parent", "support", "staff"],
         default: null,
     },
     isEmailVerified: {

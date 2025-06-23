15f46e1632383c1b48c25c9ce9a4e556f18492ce
dev06tech <dev08.tech@gmail.com>
2025-06-23 15:29:14 +0530

    fix:removed class controllers from the file

--- controllers/slate/School.controller.js
+++ controllers/slate/School.controller.js
@@ -1,6 +1,6 @@
+const { default: httpStatus } = require('http-status');
 
 const School = require('../../models/allow-box/school.model');
-const Class = require('../../models/allow-box/class.model');
 
 const createSchool = (schoolData) => {
     return new Promise(async (resolve, reject) => {
@@ -64,22 +64,10 @@ const getAllowBoxSchool = (id) => {
     })
 }
 
-const createClass = (classData) => {
+const updateAllowBoxSchool = (id, schoolData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const newClass = new Class(classData);
-            const created = await newClass.save();
-            resolve(created);
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-const updateClass = (id, classData) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const updated = await Class.findByIdAndUpdate(id, classData, { new: true });
+            const updated = await School.findByIdAndUpdate(id, schoolData, { new: true });
             resolve(updated);
         } catch (error) {
             reject(error);
@@ -87,43 +75,15 @@ const updateClass = (id, classData) => {
     })
 }
 
-const getClassesBySchool = (schoolId, page, limit, search) => {
-    let filter = {
-        associatedSchool: schoolId
-    };
-    if (search && search.trim() !== "") {
-        filter.name = { $regex: new RegExp(search, "i") };
-    }
-    console.log(filter);
-
+const deleteAllowBoxSchool = (id) => {
     return new Promise(async (resolve, reject) => {
         try {
-            const skip = (page - 1) * limit;
-            const [classes, totalCount] = await Promise.all([
-                Class.find(filter)
-                    .skip(skip)
-                    .limit(limit)
-                    .lean(),
-                Class.countDocuments(filter)
-            ]);
-            resolve({
-                currentPage: page,
-                totalPages: Math.ceil(totalCount / limit),
-                totalClasses: totalCount,
-                classes
-            });
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
-
-const deleteClass = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const deletedClass = await Class.findByIdAndDelete(id);
-            if (!deletedClass) {
-                return reject({ statusCode: httpStatus.NOT_FOUND, message: "Class not found" });
+            const deleted = await School.findByIdAndDelete(id);
+            if (!deleted) {
+                return reject({
+                    statusCode: httpStatus.NOT_FOUND,
+                    message: "School Not found"
+                });
             }
             resolve();
         } catch (error) {
@@ -136,8 +96,6 @@ module.exports = {
     createSchool,
     getAllowBoxSchools,
     getAllowBoxSchool,
-    createClass,
-    updateClass,
-    getClassesBySchool,
-    deleteClass
+    updateAllowBoxSchool,
+    deleteAllowBoxSchool
 }

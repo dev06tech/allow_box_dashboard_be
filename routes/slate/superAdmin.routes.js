996ff6630ec16e29e541b551a79aa18610d85640
dev06tech <dev08.tech@gmail.com>
2025-06-23 15:31:41 +0530

    fix:reomed school and class routes from the file

--- routes/slate/superAdmin.routes.js
+++ routes/slate/superAdmin.routes.js
@@ -11,15 +11,11 @@ const { validateSchoolRegistration,
     validateUserRoleData
 } = require('../../middlewares/validations/slate/user.validations');
 
-const { validateClass } = require('../../middlewares/validations/allow-box/class.validations');
-
 const { superAdminAuth } = require("../../middlewares/slate/superAdminAuth");
 
 const slateSuperAdminController = require("../../controllers/slate/SuperAdmin.controller");
 const allowBoxSchoolController = require("../../controllers/slate/School.controller");
 const slateUserController = require("../../controllers/slate/User.controller");
-const emailerService = require('../../services/mailsender.service')
-
 
 router.post("/register", validateRegistration, async (req, res, next) => {
     const { fullName, email, password } = req.body;
@@ -144,53 +140,5 @@ router.delete("/allow-box-users/delete", validateUserId, superAdminAuth, async (
     }
 })
 
-// allowbox classes related routes
-router.post("/allow-box-class", superAdminAuth, validateClass, async (req, res, next) => {
-    try {
-        const createdClass = await allowBoxSchoolController.createClass(req.body);
-        res.status(httpStatus.CREATED).json(createdClass);
-    } catch (error) {
-        next(error);
-    }
-})
 
-router.put("/allow-box-class/:classId", superAdminAuth, validateClass, async (req, res, next) => {
-    try {
-        const updatedClass = await allowBoxSchoolController.updateClass(req.params.classId, req.body);
-        res.status(httpStatus.OK).json(updatedClass);
-    } catch (error) {
-        next(error);
-    }
-})
-
-router.get("/allow-box-classes/:schoolId", superAdminAuth, async (req, res, next) => {
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
-    const search = req.query.search || "";
-    if (isNaN(page) || isNaN(limit) || page <= 0 || limit <= 0) {
-        return res.status(httpStatus.BAD_REQUEST).json({
-            message: "Invalid pagination parameters. 'page' and 'limit' must be positive numbers."
-        });
-    }
-    try {
-        const classes = await allowBoxSchoolController.getClassesBySchool(req.params.schoolId, page, limit, search);
-        console.log(classes);
-        
-        if (classes.totalClasses === 0) {
-            return res.status(httpStatus.NOT_FOUND).json({ message: "No classes found" });
-        }
-        res.status(httpStatus.OK).json(classes);
-    } catch (error) {
-        next(error);
-    }
-})
-
-router.delete("/allow-box-class/:classId", superAdminAuth, async (req, res, next) => {
-    try {
-        await allowBoxSchoolController.deleteClass(req.params.classId);
-        res.status(httpStatus.OK).send();
-    } catch (error) {
-        next(error);
-    }
-})
 module.exports = router;

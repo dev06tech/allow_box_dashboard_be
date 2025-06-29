03add29b5141b8336080ce1c26aa0ef93b914582
dev06tech <dev08.tech@gmail.com>
2025-06-29 12:44:46 +0530

    fix:revised student details model

--- models/allow-box/studentDetails.model.js
+++ models/allow-box/studentDetails.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { applyTimestamps } = require("./user.model");
 
 const studentDetailsSchema = new mongoose.Schema({
     studentId: {
@@ -7,15 +8,24 @@ const studentDetailsSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-    class: {
+    currentClass: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Class',
         required: true
+    },
+    dateOfBirth: {
+        type: Date
+    },
+    gender: {
+        type: String
     },
     rollNumber: {
         type: Number
     },
     parentName: {
+        type: String
+    },
+    parentPhone: {
         type: String
     },
     parentId: {
@@ -28,22 +38,52 @@ const studentDetailsSchema = new mongoose.Schema({
     photoUrl: {
         type: String
     },
-    marks: [{
-        subject: {
-            type: String
+    academics: [
+        {
+            academicYear: {
+                type: String,
+                required: true,
+            },
+            classId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Class',
+                required: true,
+            },
+            promotedToNext: {
+                type: Boolean,
+                default: false,
+            },
+            marks: [
+                {
+                    subject: {
+                        type: String
+                    },
+                    nameOfExam: {
+                        type: String
+                    },
+                    obtainedMarks: {
+                        type: Number
+                    },
+                    totalMarks: {
+                        type: Number
+                    },
+                    examDate: {
+                        type: Date
+                    },
+                    isPassed: {
+                        type: Boolean
+                    },
+                },
+            ],
         },
-        examDate: {
-            type: Date
-        },
-        score: {
-            type: Number
-        },
-        isPassed: {
-            type: Boolean
-        }
-    }],
+    ],
 
 });
+StudentDetails.set("timestamps", true);
+
+StudentDetails.index({ studentId: 1 });
+StudentDetails.index({ class: 1 });
+StudentDetails.index({ parentId: 1 });
 
 const StudentDetails = mongoose.model("StudentDetails", studentDetailsSchema);
 

30b9e1f04aee73c83152e75e4857d7ba881cfbbe
dev06tech <dev08.tech@gmail.com>
2025-06-29 23:46:23 +0530

    feat:added student details controller

---
+++ controllers/allow-box/StudentDetails.controller.js
@@ -0,0 +1,63 @@
+const { default: httpStatus } = require('http-status');
+const ApiError = require('../../utils/ApiError');
+const StudentDetails = require('../../models/allow-box/studentDetails.model');
+const User = require('../../models/allow-box/user.model');
+const { log } = require('winston');
+
+
+const createStudentDetails = async (requester, studentData) => {
+    try {
+        const isValidStudent = await User.findOne({
+            _id: studentData.studentId,
+            associatedSchool: requester.associatedSchool
+        });
+        if (!isValidStudent) {
+            throw new ApiError(httpStatus.NOT_FOUND, "Student not found");
+        }
+        const studentDetails = new StudentDetails(studentData);
+        await studentDetails.save();
+        return studentDetails;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const updateStudentDetails = async (requester, studentData) => {
+    console.log(studentData);
+    
+    try {
+        const isValidStudent = await User.findOne({
+            _id: studentData.studentId,
+            associatedSchool: requester.associatedSchool
+        });
+        if (!isValidStudent) {
+            throw new ApiError(httpStatus.NOT_FOUND, "Student not found");
+        }
+        const studentDetails = await StudentDetails.findOneAndUpdate({ studentId: studentData.studentId }, studentData, { new: true });
+        return studentDetails;
+    } catch (error) {
+        throw error;
+    }
+};
+
+const deleteStudentDetails = async (requester, studentId) => {
+    try {
+        const isValidStudent = await User.findOne({
+            _id: studentId,
+            associatedSchool: requester.associatedSchool
+        });
+        if (!isValidStudent) {
+            throw new ApiError(httpStatus.NOT_FOUND, "Student not found");
+        }
+        const studentDetails = await StudentDetails.findOneAndDelete({ studentId });
+        return studentDetails;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = {
+    createStudentDetails,
+    updateStudentDetails,
+    deleteStudentDetails
+}

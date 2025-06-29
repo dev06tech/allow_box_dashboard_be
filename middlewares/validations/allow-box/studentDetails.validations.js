b0f2ecab67463ad0e80e05a25142d646e0100731
dev06tech <dev08.tech@gmail.com>
2025-06-29 12:53:19 +0530

    fix:student details validation middleware added

---
+++ middlewares/validations/allow-box/studentDetails.validations.js
@@ -0,0 +1,14 @@
+const { default: httpStatus } = require('http-status');
+const { studentDetailsSchema } = require('../../../utils/validations/allow-box/studentDetails');
+
+const validatestudentDetails = (req, res, next) => {
+    const { error } = studentDetailsSchema.validate(req.body);
+    if (error) {
+        return res.status(httpStatus.BAD_REQUEST).json({ message: error.details[0].message });
+    }
+    next();
+}
+
+module.exports = {
+    validatestudentDetails
+}
